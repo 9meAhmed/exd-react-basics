f7b6d7eec11df5b0a87e2903fcdb567bbba14b3e
Noman Ahmed <nahmed@mcr-systems.co.uk>
2026-07-20 11:33:45 +0500

    basic cart created

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
 import Cart from './Cart.jsx'
+import BasicCart from './components/BasicCart.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Cart />
+    {/* <Cart /> */}
+    <BasicCart />
+
   </StrictMode>,
 )
